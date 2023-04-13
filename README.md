23cc4b7845792ea2acbb9dc4862efd4a3d243573
mrleafaro <91629924+mrleafaro@users.noreply.github.com>
2023-04-13 17:31:57 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# caogiga
+# cachorrao
---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyALK45NYjG6SrFbs1GyJ_Q1jNNPXODjHZI",
+  authDomain: "cachorroloko-5858a.firebaseapp.com",
+  databaseURL: "https://cachorroloko-5858a-default-rtdb.firebaseio.com",
+  projectId: "cachorroloko-5858a",
+  storageBucket: "cachorroloko-5858a.appspot.com",
+  messagingSenderId: "94084383273",
+  appId: "1:94084383273:web:d688a9451fcf331aeb9238",
+};
